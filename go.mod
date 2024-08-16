97fa7196dfa752bb927c610243ff7395cfd7a5b4
Mohammad Reza Fadaei <mohrezfadaei@gmail.com>
2024-08-16 21:38:09 +0330

    refactor(config): remove config

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.23.0
 require (
 	github.com/mattn/go-sqlite3 v1.14.22
 	github.com/spf13/cobra v1.8.1
-	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"github.com/mohrezfadaei/projinit/cmd"
-	"github.com/mohrezfadaei/projinit/config"
 	"github.com/mohrezfadaei/projinit/internal/db"
 )
 
 func main() {
 	db.InitDB("projinit.db")
 	db.Migrate()
-	config.LoadConfig("config/config.yaml")
 	cmd.Execute()
 }
